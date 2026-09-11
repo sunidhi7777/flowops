7bc7b9b5f15730ace35d976de04a9b3638c0c94b
sunidhi7777 <sunidhi777777@gmail.com>
2026-09-11 20:00:20 +0530

    Initial FlowOps project

---
+++ src/main/java/com/flowops/FlowopsApplication.java
@@ -0,0 +1,13 @@
+package com.flowops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowopsApplication.class, args);
+	}
+
+}
